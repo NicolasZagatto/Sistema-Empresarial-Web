24e494e12f8c042cd27c394870fa2560b5ea2ee1
Nicolas Bueno Zagatto <92872148+NicolasZagatto@users.noreply.github.com>
2024-03-04 22:08:11 -0300

    Add files via upload

---
+++ Projeto/js.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function () {
+    // Inicia o carrossel e configura a transição automática a cada 4 segundos
+    $('#bannerCarousel').carousel({
+        interval: 4000 // 4 segundos
+    });
+});
